82edb4f9e3524195e971b41b5c45646e7be56437
Andrew Huynh <andrew@spyglass.fyi>
2023-09-19 14:06:08 +0200

    updating migration dependency and making sure things work on sqlite vs postgres

--- migration/src/m20230919_115012_create_embedding_table.rs
+++ migration/src/m20230919_115012_create_embedding_table.rs
@@ -1,4 +1,4 @@
-use sea_orm_migration::prelude::*;
+use sea_orm_migration::{prelude::*, sea_orm::DatabaseBackend};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -6,33 +6,61 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .create_table(
-                Table::create()
-                    .table(Embeddings::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(Embeddings::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
-                    )
-                    .col(ColumnDef::new(Embeddings::Uuid).string().not_null())
-                    .col(ColumnDef::new(Embeddings::DocumentId).string().not_null())
-                    .col(ColumnDef::new(Embeddings::Segment).integer().not_null())
-                    .col(ColumnDef::new(Embeddings::Content).string().not_null())
-                    .col(
-                        ColumnDef::new(Embeddings::Vector)
-                            .array(ColumnType::Float)
-                            .not_null(),
-                    )
-                    .col(ColumnDef::new(Embeddings::Metadata).json().null())
-                    .col(ColumnDef::new(Embeddings::CreatedAt).date_time().not_null())
-                    .col(ColumnDef::new(Embeddings::UpdatedAt).date_time().not_null())
-                    .to_owned(),
-            )
-            .await?;
+        let backend = manager.get_database_backend();
+
+        if backend == DatabaseBackend::Sqlite {
+            manager
+                .create_table(
+                    Table::create()
+                        .table(Embeddings::Table)
+                        .if_not_exists()
+                        .col(
+                            ColumnDef::new(Embeddings::Id)
+                                .integer()
+                                .not_null()
+                                .auto_increment()
+                                .primary_key(),
+                        )
+                        .col(ColumnDef::new(Embeddings::Uuid).string().not_null())
+                        .col(ColumnDef::new(Embeddings::DocumentId).string().not_null())
+                        .col(ColumnDef::new(Embeddings::Segment).integer().not_null())
+                        .col(ColumnDef::new(Embeddings::Content).string().not_null())
+                        .col(ColumnDef::new(Embeddings::Vector).json().not_null())
+                        .col(ColumnDef::new(Embeddings::Metadata).json().null())
+                        .col(ColumnDef::new(Embeddings::CreatedAt).date_time().not_null())
+                        .col(ColumnDef::new(Embeddings::UpdatedAt).date_time().not_null())
+                        .to_owned(),
+                )
+                .await?;
+        } else {
+            manager
+                .create_table(
+                    Table::create()
+                        .table(Embeddings::Table)
+                        .if_not_exists()
+                        .col(
+                            ColumnDef::new(Embeddings::Id)
+                                .integer()
+                                .not_null()
+                                .auto_increment()
+                                .primary_key(),
+                        )
+                        .col(ColumnDef::new(Embeddings::Uuid).string().not_null())
+                        .col(ColumnDef::new(Embeddings::DocumentId).string().not_null())
+                        .col(ColumnDef::new(Embeddings::Segment).integer().not_null())
+                        .col(ColumnDef::new(Embeddings::Content).string().not_null())
+                        .col(
+                            ColumnDef::new(Embeddings::Vector)
+                                .array(ColumnType::Float)
+                                .not_null(),
+                        )
+                        .col(ColumnDef::new(Embeddings::Metadata).json().null())
+                        .col(ColumnDef::new(Embeddings::CreatedAt).date_time().not_null())
+                        .col(ColumnDef::new(Embeddings::UpdatedAt).date_time().not_null())
+                        .to_owned(),
+                )
+                .await?;
+        }
 
         Ok(())
     }
